390de80d4ddc66101d34b5da9cd0958956a26c4f
mazarafa <arruda.rafa.ra@gmail.com>
2021-04-15 16:44:04 -0400

    esta funcionando aleotoriamente, seloco

--- campo.asm
+++ campo.asm
@@ -1,68 +1,88 @@
+.data
 .LC2:
-        .string "SITUACAO DO CAMPO MINADO"
+        .asciz "SITUACAO DO CAMPO MINADO"
 .LC3:
-        .string " 8 "
+        .ascii " 8 "
 .LC4:
-        .string "%d "
+        .ascii "%d "
 .LC5:
         .string " %d "
 .LC6:
         .string ""
+
+
+.LC0:
+        .word 0, 9, 9, 9, 0, 9, 0, 9
+        .word 0, 0, 0, 0, 0, 0, 9, 9
+        .word 0, 0, 9, 9, 0, 0, 0, 0
+        .word 0, 0, 0, 0, 0, 0, 0, 9
+        .word 0, 9, 0, 9, 9, 0, 9, 0
+        .word 0, 9, 0, 0, 0, 9, 9, 9
+        .word 0, 0, 9, 0, 0, 0, 0, 0
+        .word 9, 0, 0, 9, 0, 9, 0, 9
+
+.LC1:
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+        .word  -1, -1, -1, -1, -1, -1, -1, -1
+
+
+        
+.text
 mostra_campo:
-        addi    sp,sp,-64
-        sd      ra,56(sp)
-        sd      s0,48(sp)
-        addi    s0,sp,64
-        sd      a0,-40(s0)
-        sd      a1,-48(s0)
-        mv      a5,a2
-        mv      a4,a3
-        sw      a5,-52(s0)
-        mv      a5,a4
-        sw      a5,-56(s0)
+        addi    sp,sp,-48
+        sw      ra,44(sp)
+        sw      s0,40(sp)
+        addi    s0,sp,48
+        sw      a0,-36(s0)
+        sw      a1,-40(s0)
+        sw      a2,-44(s0)
+        sw      a3,-48(s0)
         lui     a5,%hi(.LC2)
         addi    a0,a5,%lo(.LC2)
-        call    puts
+        #ecall    puts
         sw      zero,-20(s0)
         j       .L2
 .L8:
         sw      zero,-24(s0)
         j       .L3
 .L7:
-        lw      a5,-56(s0)
-        sext.w  a4,a5
+        lw      a4,-48(s0)
         li      a5,1
         bne     a4,a5,.L4
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,9
         bne     a4,a5,.L4
         lui     a5,%hi(.LC3)
         addi    a0,a5,%lo(.LC3)
-        call    printf
+       # call    printf
         j       .L5
 .L4:
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-48(s0)
+        lw      a4,-40(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,-1
         bne     a4,a5,.L6
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-48(s0)
+        lw      a4,-40(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
@@ -71,12 +91,12 @@ mostra_campo:
         mv      a1,a5
         lui     a5,%hi(.LC4)
         addi    a0,a5,%lo(.LC4)
-        call    printf
+        #call    printf
         j       .L5
 .L6:
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-48(s0)
+        lw      a4,-40(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
@@ -85,251 +105,223 @@ mostra_campo:
         mv      a1,a5
         lui     a5,%hi(.LC5)
         addi    a0,a5,%lo(.LC5)
-        call    printf
+        #call    printf
 .L5:
         lw      a5,-24(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-24(s0)
 .L3:
         lw      a4,-24(s0)
-        lw      a5,-52(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
+        lw      a5,-44(s0)
         blt     a4,a5,.L7
         lui     a5,%hi(.LC6)
         addi    a0,a5,%lo(.LC6)
-        call    puts
+        #call    puts
         lw      a5,-20(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-20(s0)
 .L2:
         lw      a4,-20(s0)
-        lw      a5,-52(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
+        lw      a5,-44(s0)
         blt     a4,a5,.L8
         nop
-        ld      ra,56(sp)
-        ld      s0,48(sp)
-        addi    sp,sp,64
+        nop
+        lw      ra,44(sp)
+        lw      s0,40(sp)
+        addi    sp,sp,48
         jr      ra
+        
 calcula_bomba:
         addi    sp,sp,-48
-        sd      s0,40(sp)
+        sw      s0,44(sp)
         addi    s0,sp,48
-        sd      a0,-40(s0)
-        mv      a5,a1
-        sw      a5,-44(s0)
-        lw      a5,-44(s0)
-        addiw   a5,a5,-1
+        sw      a0,-36(s0)
+        sw      a1,-40(s0)
+        lw      a5,-40(s0)
+        addi    a5,a5,-1
         sw      a5,-32(s0)
         sw      zero,-20(s0)
         j       .L10
+
 .L23:
         sw      zero,-24(s0)
         j       .L11
+
 .L22:
         sw      zero,-28(s0)
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         beq     a4,a5,.L24
         lw      a5,-20(s0)
-        sext.w  a5,a5
-        beqz    a5,.L14
+        beq     a5,zero,.L14
         lw      a5,-24(s0)
-        sext.w  a5,a5
-        beqz    a5,.L14
-        lw      a5,-20(s0)
+        beq     a5,zero,.L14
+        lw      a4,-20(s0)
+        li      a5,134217728
+        addi    a5,a5,-1
+        add     a5,a4,a5
         slli    a5,a5,5
-        addi    a5,a5,-32
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,-1
-        sext.w  a5,a5
+        addi    a5,a5,-1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L14
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L14:
         lw      a5,-20(s0)
-        sext.w  a5,a5
-        beqz    a5,.L15
-        lw      a5,-20(s0)
+        beq     a5,zero,.L15
+        lw      a4,-20(s0)
+        li      a5,134217728
+        addi    a5,a5,-1
+        add     a5,a4,a5
         slli    a5,a5,5
-        addi    a5,a5,-32
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L15
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L15:
         lw      a5,-20(s0)
-        sext.w  a5,a5
-        beqz    a5,.L16
+        beq     a5,zero,.L16
         lw      a4,-24(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L16
-        lw      a5,-20(s0)
+        lw      a4,-20(s0)
+        li      a5,134217728
+        addi    a5,a5,-1
+        add     a5,a4,a5
         slli    a5,a5,5
-        addi    a5,a5,-32
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,1
-        sext.w  a5,a5
+        addi    a5,a5,1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L16
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L16:
         lw      a5,-24(s0)
-        sext.w  a5,a5
-        beqz    a5,.L17
+        beq     a5,zero,.L17
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,-1
-        sext.w  a5,a5
+        addi    a5,a5,-1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L17
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L17:
         lw      a4,-24(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L18
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,1
-        sext.w  a5,a5
+        addi    a5,a5,1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L18
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L18:
         lw      a4,-20(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L19
         lw      a5,-24(s0)
-        sext.w  a5,a5
-        beqz    a5,.L19
+        beq     a5,zero,.L19
         lw      a5,-20(s0)
         addi    a5,a5,1
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,-1
-        sext.w  a5,a5
+        addi    a5,a5,-1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L19
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L19:
         lw      a4,-20(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L20
         lw      a5,-20(s0)
         addi    a5,a5,1
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L20
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L20:
         lw      a4,-20(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L21
         lw      a4,-24(s0)
         lw      a5,-32(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
         beq     a4,a5,.L21
         lw      a5,-20(s0)
         addi    a5,a5,1
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
-        addiw   a5,a5,1
-        sext.w  a5,a5
+        addi    a5,a5,1
         slli    a5,a5,2
         add     a5,a4,a5
-        lw      a5,0(a5)
-        mv      a4,a5
+        lw      a4,0(a5)
         li      a5,8
         bne     a4,a5,.L21
         lw      a5,-28(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-28(s0)
 .L21:
         lw      a5,-20(s0)
         slli    a5,a5,5
-        ld      a4,-40(s0)
+        lw      a4,-36(s0)
         add     a4,a4,a5
         lw      a5,-24(s0)
         slli    a5,a5,2
@@ -341,235 +333,101 @@ calcula_bomba:
         nop
 .L13:
         lw      a5,-24(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-24(s0)
 .L11:
         lw      a4,-24(s0)
-        lw      a5,-44(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
+        lw      a5,-40(s0)
         blt     a4,a5,.L22
         lw      a5,-20(s0)
-        addiw   a5,a5,1
+        addi    a5,a5,1
         sw      a5,-20(s0)
 .L10:
         lw      a4,-20(s0)
-        lw      a5,-44(s0)
-        sext.w  a4,a4
-        sext.w  a5,a5
+        lw      a5,-40(s0)
         blt     a4,a5,.L23
         nop
-        ld      s0,40(sp)
+        nop
+        lw      s0,44(sp)
         addi    sp,sp,48
         jr      ra
-.LC7:
-        .string "\nDigite as coordenadas do campo minado (coluna)(linha): "
-.LC8:
-        .string "%d"
-.LC9:
-        .string "\nA BOMBA EXPLODIU! VOCE PERDEU!"
-.LC0:
-        .word   0
-        .word   9
-        .word   9
-        .word   9
-        .word   0
-        .word   9
-        .word   0
-        .word   9
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   9
-        .word   9
-        .word   0
-        .word   0
-        .word   9
-        .word   9
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   9
-        .word   0
-        .word   9
-        .word   0
-        .word   9
-        .word   9
-        .word   0
-        .word   9
-        .word   0
-        .word   0
-        .word   9
-        .word   0
-        .word   0
-        .word   0
-        .word   9
-        .word   9
-        .word   9
-        .word   0
-        .word   0
-        .word   9
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   0
-        .word   9
-        .word   0
-        .word   0
-        .word   9
-        .word   0
-        .word   9
-        .word   0
-        .word   9
-.LC1:
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
-        .word   -1
+#.LC7:
+ #       .string "\nDigite as coordenadas do campo minado (coluna)(linha): "
+#.LC8:
+     #   .string "%d"
+#.LC9:
+      #  .string "\nA BOMBA EXPLODIU! VOCE PERDEU!"
+
+
 main:
         addi    sp,sp,-544
-        sd      ra,536(sp)
-        sd      s0,528(sp)
+        sw      ra,540(sp)
+        sw      s0,536(sp)
         addi    s0,sp,544
         lui     a5,%hi(.LC0)
-        addi    a4,s0,-280
-        addi    a5,a5,%lo(.LC0)
-        li      a3,256
-        mv      a2,a3
-        mv      a1,a5
-        mv      a0,a4
-        call    memcpy
+        addi    a4,a5,%lo(.LC0)
+        addi    a5,s0,-280
+        mv      a3,a4
+        li      a4,256
+        mv      a2,a4
+        mv      a1,a3
+        mv      a0,a5
+        #call    memcpy
         lui     a5,%hi(.LC1)
-        addi    a4,s0,-536
-        addi    a5,a5,%lo(.LC1)
-        li      a3,256
-        mv      a2,a3
-        mv      a1,a5
-        mv      a0,a4
-        call    memcpy
+        addi    a4,a5,%lo(.LC1)
+        addi    a5,s0,-536
+        mv      a3,a4
+        li      a4,256
+        mv      a2,a4
+        mv      a1,a3
+        mv      a0,a5
+       # call    memcpy
         li      a5,8
         sw      a5,-20(s0)
         sw      zero,-24(s0)
-        lw      a4,-20(s0)
         addi    a5,s0,-280
-        mv      a1,a4
+        lw      a1,-20(s0)
         mv      a0,a5
         call    calcula_bomba
 .L30:
-        lw      a3,-24(s0)
-        lw      a2,-20(s0)
         addi    a4,s0,-536
         addi    a5,s0,-280
+        lw      a3,-24(s0)
+        lw      a2,-20(s0)
         mv      a1,a4
         mv      a0,a5
         call    mostra_campo
-        lui     a5,%hi(.LC7)
-        addi    a0,a5,%lo(.LC7)
-        call    puts
+        #lui     a5,%hi(.LC7)
+        #addi    a0,a5,%lo(.LC7)
+        #call    puts
         addi    a5,s0,-544
         mv      a1,a5
-        lui     a5,%hi(.LC8)
-        addi    a0,a5,%lo(.LC8)
-        call    __isoc99_scanf
+       # lui     a5,%hi(.LC8)
+        #addi    a0,a5,%lo(.LC8)
+        #call    scanf
         addi    a5,s0,-540
         mv      a1,a5
-        lui     a5,%hi(.LC8)
-        addi    a0,a5,%lo(.LC8)
-        call    __isoc99_scanf
+        #lui     a5,%hi(.LC8)
+        #addi    a0,a5,%lo(.LC8)
+        #call    scanf
         lui     a5,%hi(.LC6)
         addi    a0,a5,%lo(.LC6)
-        call    puts
-        lw      a4,-544(s0)
-        lw      a5,-20(s0)
-        sext.w  a5,a5
-        blt     a5,a4,.L33
-        lw      a4,-540(s0)
-        lw      a5,-20(s0)
-        sext.w  a5,a5
-        blt     a5,a4,.L33
-        lw      a5,-540(s0)
-        beqz    a5,.L33
+        #call    puts
         lw      a5,-544(s0)
-        beqz    a5,.L33
+        lw      a4,-20(s0)
+        blt     a4,a5,.L33
         lw      a5,-540(s0)
-        addiw   a5,a5,-1
-        sext.w  a5,a5
+        lw      a4,-20(s0)
+        blt     a4,a5,.L33
+        lw      a5,-540(s0)
+        beq     a5,zero,.L33
+        lw      a5,-544(s0)
+        beq     a5,zero,.L33
+        lw      a5,-540(s0)
+        addi    a5,a5,-1
         sw      a5,-540(s0)
         lw      a5,-544(s0)
-        addiw   a5,a5,-1
-        sext.w  a5,a5
+        addi    a5,a5,-1
         sw      a5,-544(s0)
         lw      a4,-540(s0)
         lw      a5,-544(s0)
@@ -594,19 +452,17 @@ main:
         slli    a5,a5,2
         addi    a4,s0,-16
         add     a5,a4,a5
-        lw      a5,-264(a5)
-        mv      a4,a5
+        lw      a4,-264(a5)
         li      a5,8
         bne     a4,a5,.L30
         li      a5,1
         sw      a5,-24(s0)
-        lui     a5,%hi(.LC9)
-        addi    a0,a5,%lo(.LC9)
-        call    puts
-        lw      a3,-24(s0)
-        lw      a2,-20(s0)
+      
+        #call    puts
         addi    a4,s0,-536
         addi    a5,s0,-280
+        lw      a3,-24(s0)
+        lw      a2,-20(s0)
         mv      a1,a4
         mv      a0,a5
         call    mostra_campo
@@ -617,7 +473,7 @@ main:
 .L32:
         li      a5,0
         mv      a0,a5
-        ld      ra,536(sp)
-        ld      s0,528(sp)
+        lw      ra,540(sp)
+        lw      s0,536(sp)
         addi    sp,sp,544
         jr      ra
